bd534d24d8f391da52ebe8f95cca587413ea32d7
“OGWJ” <owenglynwilliamjenkins@gmail.com>
2021-09-06 15:21:52 +0100

    added BandCard component and likes button

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<h1>hello ffs</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
